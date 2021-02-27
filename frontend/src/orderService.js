420be26323f4254cfc1983eb8424a0591f47e6dc
ChrisJabb21 <chrisjabbour94@gmail.com>
2021-02-26 22:09:31 -0500

    added and tested frontend api implementation and order screen

--- frontend/src/orderService.js
+++ frontend/src/orderService.js
@@ -1,47 +1,83 @@
 import axios from "axios";
 import { apiUrl } from "./config";
-import { getUserInfo } from "./localStorage"
+import { getUserInfo } from "./localStorage";
+
+/* Backend api endpoint mappings */
 
 /* Order api mappings */
 
-export const createOrder = async(order) => {
-try{
+export const createOrder = async (order) => {
+  try {
+    const { token } = getUserInfo();
+    const response = await axios({
+      url: `${apiUrl}/api/orders`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      data: order,
+    });
+    if (response.statusText !== "Created") {
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (err) {
+    return { error: err.response ? err.response.data.message : err.message };
+  }
+};
+
+export const getOrder = async (id) => {
+  try {
+    const { token } = getUserInfo();
+    const response = await axios({
+      url: `${apiUrl}/api/orders/${id}`,
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (response.statusText !== "OK") {
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (err) {
+    return { error: err.message };
+  }
+};
+
+/* Paypal */
+export const getPaypalClientId = async () => {
+  const response = await axios({
+    url: `${apiUrl}/api/paypal/clientId`,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.statusText !== "OK") {
+    throw new Error(response.data.message);
+  }
+  return response.data.clientId;
+};
+
+export const payOrder = async(orderId, paymentResult) => {
+  try{
     const {token} = getUserInfo();
     const response = await axios({
-        url: `${apiUrl}/api/orders`,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-        },
-        data: order,
+      url: `${apiUrl}/api/orders/${orderId}/pay`,
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      data: paymentResult,
     });
-    if(response.statusText !== 'Created'){
-        throw new Error(response.data.message);
-    }
+    if(response.statusText !== "OK"){
+      throw new Error(response.data.message);
+    } 
     return response.data;
-}catch(err){
+  } catch(err){
     return { error: err.response ? err.response.data.message : err.message };
-    };
-}
-
-export const getOrder = async(id) => {
-    try{ 
-        const { token } = getUserInfo();
-    const response = await axios({
-        url: `${apiUrl}/api/orders/${id}`,
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-        },    
-    });
-    if(response.statusText !== 'OK'){
-        throw new Error(response.data.message);
-    }
-    return response.data;
-}
-    catch(err){
-        return { error: err.message };
-    }
+  } 
 };
--- frontend/src/views/OrderScreen.js
+++ frontend/src/views/OrderScreen.js
@@ -1,26 +1,90 @@
-import { getOrder } from "../orderService";
-import { formatter, parseRequestUrl } from "../utils";
+import { getOrder, getPaypalClientId, payOrder } from "../orderService";
+import { formatter, hideLoading, parseRequestUrl, rerender, showLoading, showMessage } from "../utils";
 
+const addPaypalSdk = async (totalPrice) => {
+  const clientId = await getPaypalClientId();
+  showLoading();
+  if(!window.paypal){
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://www.paypalobjects.com/api/checkout.js';
+    script.async = true;
+    script.onload = () => handlePayment(clientId, Number(totalPrice).toFixed(2));
+    document.body.appendChild(script);
+  } else {
+    handlePayment(clientId, Number(totalPrice).toFixed(2));
+  }
+};
+
+const handlePayment = (clientId, totalPrice) => {
+  window.paypal.Button.render({
+    env: 'sandbox',
+    client:{
+      sandbox: clientId,
+      production: '',
+    },
+    locale: 'en_US',
+    style: {
+      size: 'responsive',
+      color: 'gold',
+      shape: 'pill',
+    },
+
+    commit: true,
+    payment(data, actions){
+      return actions.payment.create({
+        transactions: [
+          {
+            amount: {
+              total: Number(totalPrice).toFixed(2),
+              currency: 'USD',
+            },
+          },
+        ],
+      });
+    },
+    onAuthorize(data,actions){
+      return actions.payment.execute().then(async() =>{
+        showLoading();
+        await payOrder(parseRequestUrl().id, {
+          orderID: data.orderID,
+          payerID: data.payerID,
+          paymentID: data.paymentID,
+        })
+        // call pay order function
+        hideLoading();
+        showMessage('Payment was successful.', () => {
+          rerender(OrderScreen);          
+        }); 
+      });
+    },
+  }, 
+  '#paypal-button'
+  ).then(() => {
+    hideLoading();
+  });
+};
 const OrderScreen = {
-  after_render:  async() => {},
-  render: async() => {
+  after_render: async () => {},
+  render: async () => {
     const request = parseRequestUrl();
     const {
-        _id,
-        shipping,
-        payment,
-        itemsPrice,
-        orderItems,
-        shippingPrice,
-        taxPrice,
-        totalPrice,
-        isDelivered,
-        deliveredAt,
-        isPaid,
-        paidAt,
-
+      _id,
+      shipping,
+      payment,
+      itemsPrice,
+      orderItems,
+      shippingPrice,
+      taxPrice,
+      totalPrice,
+      isDelivered,
+      deliveredAt,
+      isPaid,
+      paidAt,
     } = await getOrder(request.id);
-
+    if(!isPaid){
+      addPaypalSdk(Number(totalPrice).toFixed(2));
+    }
     return `
     <div>
     <h1>Order Confirmation</h1>
@@ -36,7 +100,8 @@ const OrderScreen = {
                     ${shipping.address}, ${shipping.city},  ${shipping.state}, 
                     ${shipping.postalCode}, ${shipping.country}. 
                     </div>
-                    ${isDelivered 
+                    ${
+                      isDelivered
                         ? `<div class="success">Delivered at ${deliveredAt}</div>`
                         : `<div class="error"> Not delivered</div>`
                     }
@@ -49,7 +114,8 @@ const OrderScreen = {
             Payment Method : ${payment.paymentMethod}
          
             </div>
-            ${isPaid 
+            ${
+              isPaid
                 ? `<div class="success">Paid at ${paidAt}</div>`
                 : `<div class="error"> Not paid</div>`
             }
@@ -62,8 +128,9 @@ const OrderScreen = {
                 <h2>Shopping Cart</h2>
                 <div>Price</div>
             </li>
-            ${orderItems.map(
-              (item) => `
+            ${orderItems
+              .map(
+                (item) => `
                     <li>
                     <div class="cart-image">
                     <img src="${item.image}" alt="${item.name}" />
@@ -76,23 +143,30 @@ const OrderScreen = {
                         </div>
                         <div class ="cart-price"> $${item.price}</div>
                     </li>
-                        `).join('\n')}
+                        `
+              )
+              .join("\n")}
             </ul>
         </div>
     </div>
     <div class="order-action">
         <ul>
             <li>
-            <h2>Order Summary</h2>
-            </li>
-            <li><div>Items</div><div>${formatter.format(itemsPrice)}</div>
-            </li>
-            <li><div>Shipping</div><div>${formatter.format(shippingPrice)}</div>
-            </li>
-            <li><div>Tax</div><div>${formatter.format(taxPrice)}</div>
-            </li>
-            <li><div class="total">Order Total ${formatter.format(totalPrice)}</div>
-            </li>
+              <h2>Order Summary</h2>
+              </li>
+              <li><div>Items</div><div>${formatter.format(itemsPrice)}</div>
+              </li>
+              <li><div>Shipping</div><div>${formatter.format(shippingPrice)}</div>
+              </li>
+              <li><div>Tax</div><div>${formatter.format(taxPrice)}</div>
+              </li>
+              <li><div class="total">Order Total ${formatter.format(
+                totalPrice
+              )}</div>
+              </li>
+              <li>
+              <div class="fw" id="paypal-button"></div></li>
+              <li>
         </div>
     </div>
 </div>
