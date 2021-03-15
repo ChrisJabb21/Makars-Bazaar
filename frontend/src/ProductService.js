5b7d518bccc8580a4d8b9e7576a2887a0d7c8fbd
ChrisJabb21 <chrisjabbour94@gmail.com>
2021-03-15 11:51:20 -0400

    added create product implementation for frontend

--- frontend/src/ProductService.js
+++ frontend/src/ProductService.js
@@ -1,5 +1,27 @@
 import axios from "axios";
 import { apiUrl } from "./config";
+import { getUserInfo } from "./localStorage";
+import { hideLoading } from "./utils";
+
+export const createProduct = async() => {
+  try {
+    const {token} = getUserInfo();
+    const response = await axios({
+      url: `${apiUrl}/api/products`,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if(response.statusText !== 'Created'){
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (err) {
+    return { error: err.response.data.message || err.message };
+  }
+}
 
 export const getProduct = async (id) => {
   try {
@@ -29,6 +51,7 @@ export const getProducts = async () => {
         'Content-Type': 'application/json',
       },
     });
+    hideLoading();
     if (response.statusText !== 'OK') {
       throw new Error(response.data.message);
     }
--- frontend/src/views/ProductListScreen.js
+++ frontend/src/views/ProductListScreen.js
@@ -1,8 +1,14 @@
 import DashboardMenu from "../components/DashboardMenu";
-import { getProducts } from "../productService";
+import { createProduct, getProducts } from "../productService";
 
 const ProductListScreen = {
-  after_render: () => {},
+  after_render: () => {
+    document.getElementById('create-product-button')
+    .addEventListener('click', async() =>{
+      const data = await createProduct();
+      document.location.hash = `/product/${data.product._id}/edit`;
+    })
+  },
   render: async () => {
     const products = await getProducts(); 
     return `
