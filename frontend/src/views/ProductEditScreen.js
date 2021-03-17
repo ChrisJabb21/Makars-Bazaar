82c3968dad663696e5aa470f7392253ceab8fdf3
ChrisJabb21 <chrisjabbour94@gmail.com>
2021-03-17 11:05:02 -0400

    Added full form & afterrender impl for ProductEdit

--- frontend/src/views/ProductEditScreen.js
+++ frontend/src/views/ProductEditScreen.js
@@ -1,15 +1,84 @@
-import { getProduct } from "../productService";
-import { parseRequestUrl } from "../utils";
+import { getProduct, updateProduct } from "../productService";
+import { hideLoading, parseRequestUrl, showLoading, showMessage } from "../utils";
 
 const ProductEditScreen = {
-  after_render: () => {},
+  after_render: () => {
+    const request = parseRequestUrl();
+    document.getElementById("edit-product-form")
+    .addEventListener('submit', async (e) => {
+      e.preventDefault();
+      showLoading();
+      const data = await updateProduct({
+        _id: request.id,
+        name: document.getElementById('name').value,
+        category: document.getElementById('category').value,
+        image: document.getElementById('image').value, 
+        price: document.getElementById('price').value, 
+        brand: document.getElementById('brand').value, 
+        countInStock: document.getElementById('countInStock').value, 
+        description: document.getElementById('description').value, 
+      });
+      hideLoading();
+      if(data.error){
+        showMessage(data.error);
+      } else {
+        document.location.hash = '/productlist';
+      }
+    });
+  },
+
   render: async () => {
     const request = parseRequestUrl();
     const product = await getProduct(request.id);
-    return `${product.name}`;
-
+    return `
+    <div class="content">
+      <div>
+        <a class="btn btn-primary" href="/#/productlist">Back to products</a>
+      </div>
+      <div class="form-container">
+        <form id="edit-product-form">
+          <ul class="form-items">
+            <li>
+              <h1>Edit Product</h1>
+              <h2>Product Id : ${product._id.substring(0,8)}...</h2>
+            </li>
+            <li>
+              <label for="name">Name</label>
+              <input type="text" name="name" value="${product.name}" id="name" />
+            </li>  
+            <li>
+              <label for="price">Price</label>
+              <input type="number" step="any" name="price" value="${product.price}" id="price" />
+            </li>
+            <li>
+              <label for="image">Product Image (preferred size: 680px x 830px)</label>
+              <input type="text" name="image" value="${product.image}" id="image" />
+            </li>
+            <li>
+              <label for="brand">Brand</label>
+              <input type="text" name="brand" value="${product.brand}" id="brand" />
+            </li>
+            <li>
+              <label for="countInStock">Count in Stock</label>
+              <input type="text" name="countInStock" value="${product.countInStock}" id="countInStock" />
+            </li>
+            <li>
+              <label for="category">Category</label>
+              <input type="text" name="category" value="${product.category}" id="category" />
+            </li>
+            <li>
+              <label for="description">Description</label>
+              <input type="text" name="description" value="${product.description}" id="description" />
+            </li>
+            <li>
+              <button type="submit" class="btn btn-primary">Update</button>
+            </li>
+          </ul>
+        </form>
+      </div>
+    </div>
+    `;
   },
-
 };
 
 export default ProductEditScreen;
