3a4fe5dd54c2cb64a1eeedc05c8a28a68d997cbf
ChrisJabb21 <chrisjabbour94@gmail.com>
2021-03-27 20:18:02 -0400

    added comments to config js for testing npm script

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,4 @@
 export const apiUrl = 'http://localhost:5000';
-// if ran with npm serve, use
-export const clientUrl = 'http://localhost:5000';
-
+// if run with npm serve, use 5000
 // if ran with npm start, use 9002 
-// export const clientUrl = 'http://localhost:9002';
+export const clientUrl = 'http://localhost:9002';
